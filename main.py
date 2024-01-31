0f6798438e86a33ac3906416a3d79ae0cff92441
jcjoiner <jcjoiner555@gmail.com>
2024-01-31 15:45:17 -0600

    Added functions to organize code. Added an exception to catch an error.

--- main.py
+++ main.py
@@ -5,27 +5,52 @@
         - No negatives
     - Provide feedback if number is high or low
     - Only allow x amount of guesses and keep track of them
-    - Ask to play again after a win or a loss 
+    - Ask to play again after a win or a loss
 """
-
 import random
-
-rand_int = random.randint(0, 100)
-attempts = 0
-max_attempts = 5
-print(rand_int)
+import time
+import os
 
 # game loop
-while attempts < max_attempts:
-    attempts += 1
+def game_loop():
+    rand_int = random.randint(0, 100)
+    attempts = 0
+    max_attempts = 5
 
-    print(f"Attempt: {attempts} of {max_attempts}")
-    guess = int(input("Enter a guess: "))
+    while attempts < max_attempts:
+        attempts += 1
 
-    if attempts >= max_attempts:
-        print(f"Too many attempts! {rand_int} was the number.")
-    elif guess < rand_int:
-        print(f"Higher than {guess}! Guess again!")
-    else:
-        print(f"Lower than {guess}! Guess again!")
+        print(f"Attempt: {attempts} of {max_attempts}")
+        try:
+            guess = int(input("Enter a guess: "))
+        except ValueError:
+            print("Integers only. Try again.")
+            guess = int(input("Enter a guess: "))
+        print("========================================")
+       
+        if guess == rand_int:
+            print(f"You win! {rand_int} was the number!")
+            break
+        elif attempts >= max_attempts:
+            print(f"Too many attempts! {rand_int} was the number.")
+            break
+        elif guess < rand_int:
+            print(f"Higher than {guess}! Guess again!")
+        else:
+            print(f"Lower than {guess}! Guess again!")
 
+def main():
+    game_loop()
+
+    while True:
+        choice = input("Would you like to play again? (Y/N)\n> ").upper()
+        
+        if choice == "Y":
+            os.system("clear")
+            game_loop()
+        else:
+            print("Thanks for playing!")
+            time.sleep(1)
+            quit()
+
+main()
